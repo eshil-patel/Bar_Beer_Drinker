fd4ebbaf583a210c44f1fa0d9f56fbbcc5d08aaa
Eshil Patel <xeshil101@optonline.net>
2018-11-08 17:44:35 -0500

    Commit

--- Server/BarBeerDrinker/__init__.py
+++ Server/BarBeerDrinker/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask import jsonify
+from flask import make_response
+from flask import request
+import json
 
-app = Flask(__name__)
+from BarBeerDrinker import database
 
-@app.route('/')
+app= Flask(__name__)
+
+@app.route('/api/bar',methods['GET'])
+def get_bars():
+    jsonify(database.get_bars())
 def hello_world():
     return 'Hello world!'
