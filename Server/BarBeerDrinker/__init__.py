7ec8dc59007148945d566b0fb2134ac5057c0912
Eshil Patel <xeshil101@optonline.net>
2018-11-10 15:31:37 -0500

    ok

--- Server/BarBeerDrinker/__init__.py
+++ Server/BarBeerDrinker/__init__.py
@@ -1,15 +1,7 @@
 from flask import Flask
-from flask import jsonify
-from flask import make_response
-from flask import request
-import json
 
-from BarBeerDrinker import database
+app = Flask(__name__)
 
-app= Flask(__name__)
-
-@app.route('/api/bar',methods['GET'])
-def get_bars():
-    jsonify(database.get_bars())
+@app.route('/')
 def hello_world():
     return 'Hello world!'
